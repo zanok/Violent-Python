9503f89e2e9394603e6f6ad8e9138726e444bfc7
Trevor Duetsch <zanokmanza+github@gmail.com>
2013-02-25 13:24:40 -0500

    Added the Introduction folder with the unix password cracker

---
+++ Introduction/passwordcracker.py
@@ -0,0 +1,24 @@
+import sys
+
+import crypt
+
+
+def crack(hash):
+    salt = hash[0:2]
+    file = open('dictionary.txt','r')
+    for word in file.readlines():
+        word = word.strip('\n')
+        if crypt.crypt(word,'hx') == hash:
+            print '[+] The password is: ',word
+            return 
+    print '[-] Password not found.' 
+
+
+def main():
+    file = open('passwords.txt','r')
+    for word in file.readlines():
+
+
+if __name__ == "__main__":
+    print crypt.crypt('egg','hx')
+    crack('hxokzx4Ngzx5I')
